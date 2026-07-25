cca0c216d56cad918d751c1eb37153c719cd4ac2
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-07-25 15:26:29 -0400

    fix: export user signal RxDB schema from persistence module

--- src/recommendations/index.ts
+++ src/recommendations/index.ts
@@ -3,12 +3,13 @@ export {
   USER_SIGNAL_PROVENANCE,
   USER_SIGNAL_SCHEMA_VERSION,
   USER_SIGNAL_TYPES,
-  userRecommendationSignalsCollection,
   type UserRecommendationSignalDoc,
   type UserSignalEntityType,
   type UserSignalProvenance,
   type UserSignalType
 } from "./user-signal-schema";
+
+export { userRecommendationSignalsCollection } from "./user-signal-rxdb";
 
 export {
   buildUserSignalId,
