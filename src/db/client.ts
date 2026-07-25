9eeb326697b2d3e1730140599a8e468acbbbb16e
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-07-25 15:17:12 -0400

    feat: expose typed user recommendation signal collection

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,6 +2,7 @@ import { addRxPlugin, createRxDatabase, type RxCollection, type RxDatabase } fro
 import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
 import { RxDBMigrationSchemaPlugin } from 'rxdb/plugins/migration-schema';
 import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
+import type { UserRecommendationSignalDoc } from '../recommendations/user-signal-schema';
 import { collections } from './runtime-schema';
 import type {
   AuthorDoc,
@@ -31,6 +32,7 @@ export type RyuCollections = {
   searchindexdependencies: RxCollection<SearchIndexDependencyDoc>;
   fetchqueue: RxCollection<FetchQueueDoc>;
   writequeue: RxCollection<WriteQueueDoc>;
+  userrecommendationsignals: RxCollection<UserRecommendationSignalDoc>;
 };
 
 export type RyuDatabase = RxDatabase<RyuCollections>;
